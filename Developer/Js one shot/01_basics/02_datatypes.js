08f2f9da34a330006b109698f72f7a6e1fad56f1
Riya Jaiswal <riyajstar07@gmail.com>
2026-09-08 12:03:11 +0530

    Add JavaScript datatypes basics

---
+++ Developer/Js one shot/01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node js not browserr so it have diff mehtod
+
+console.log(3+3);
+
+let name = "RIYA"
+let age = 20
+let isLoggedIn = false
+
+console.table([name,age,isLoggedIn]);
+
+console.log(typeof "RIYA")
